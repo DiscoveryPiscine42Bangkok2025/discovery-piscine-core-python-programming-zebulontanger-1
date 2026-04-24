06a4f214a735bf9c62aef947a99cfc137493fb02
zebulontanger <zebulontanger@gmail.com>
2026-04-24 15:38:43 +0700

    Add files via upload

--- cell06/ex03/greetings_for_all.py
+++ cell06/ex03/greetings_for_all.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
 
+def greetings(name=None):
+    if name is None or name == '':
+        print("Hello, noble stranger.")
+    elif isinstance(name, str):
+        print(f"Hello, {name}")
+    else:
+        print("Error! It was not a name.")
+
+greetings('Alexandra')
+greetings('Wil')
+greetings()
+greetings(42)
