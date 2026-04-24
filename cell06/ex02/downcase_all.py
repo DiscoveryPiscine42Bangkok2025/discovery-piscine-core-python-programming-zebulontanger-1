57bc27bcfc5d4b85ac780fec916f2b75681b9782
zebulontanger <zebulontanger@gmail.com>
2026-04-24 15:14:31 +0700

    Add files via upload

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+import sys
 
+def downcase_it(args_list):
+    for arg in args_list:
+        word = arg.lower()
+        print(word)
+
+if len(sys.argv) > 1:
+    downcase_it(sys.argv[1:])
+else:
+    print('none')
