6f547acc2d02686244cca6079ffea2ff7d185822
zebulontanger <zebulontanger@gmail.com>
2026-04-25 11:49:52 +0700

    Add files via upload

--- cell07/ex01/family_affairs.py
+++ cell07/ex01/family_affairs.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
 
+def find_the_redheads(dupont_families_dict):
+    return [f"{first}" for first, last in dupont_families_dict.items() if last == 'red']
+
+dupont_family = {
+"florian": "red",
+"marie": "blond",
+"virginie": "brunette",
+"david": "red",
+"franck": "red"
+}
+print(find_the_redheads(dupont_family))
