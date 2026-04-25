a348365e2fed8c10e5954624678cb63acb6e18e3
zebulontanger <zebulontanger@gmail.com>
2026-04-25 11:49:16 +0700

    Add files via upload

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
 
+def array_of_names(persons_dict):
+    return [f"{first}".capitalize() + ' ' + f"{last}".capitalize() for first, last in persons_dict.items()]
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(persons))
