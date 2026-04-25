2cb5876cc380da45c95277144b4228b2785d387a
zebulontanger <zebulontanger@gmail.com>
2026-04-25 11:34:41 +0700

    Add files via upload

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+import sys
 
+args = sys.argv[1:]
+
+if args:
+    for arg in args:
+        if len(arg) > 8:
+            processed = arg[:8]
+        elif len(arg) < 8:
+            processed = arg.ljust(8, 'Z')
+        else:
+            processed = arg
+        
+        print(processed)
+else:
+    print('none')
