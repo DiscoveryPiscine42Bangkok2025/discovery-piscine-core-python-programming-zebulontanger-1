611cf834313cac96a8c8b18b3bd507584f598770
zebulontanger <zebulontanger@gmail.com>
2026-04-26 14:02:48 +0700

    Create main.py

---
+++ rush/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# ALL POSSIBLE MOVES
+# Pawn (P):
+# . . . . . . .
+# . . . . . . .
+# . . X . X . .
+# . . . P . . .
+# . . . . . . .
+# . . . . . . .
+# . . . . . . .
+# Bishop (B):
+# X . . . . . X
+# . X . . . X .
+# . . X . X . .
+# . . . B . . .
+# . . X . X . .
+# . X . . . X .
+# X . . . . . X
+# Rook (R):
+# . . . X . . .
+# . . . X . . .
+# . . . X . . .
+# X X X R X X X
+# . . . X . . .
+# . . . X . . .
+# . . . X . . .
+# Queen (Q)
+# X . . X . . X
+# . X . X . X .
+# . . X X X . .
+# X X X Q X X X
+# . . X X X . .
+# . X . X . X .
+# X . . X . . X
+
+from checkmate import checkmate
+
+def main():
+    board = """\
+..
+.K\
+"""
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
