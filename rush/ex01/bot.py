b335d19b75afc7eca14a79c44c701c54a9f87639
zebulontanger <zebulontanger@gmail.com>
2026-04-27 23:51:23 +0700

    Update bot.py

--- rush/ex01/bot.py
+++ rush/ex01/bot.py
@@ -1,13 +1,11 @@
-import pygame
 import random
 import pickle
 
-WIDTH, HEIGHT = 600, 600
-SQ_SIZE = WIDTH // 8
-WHITE = (240, 217, 181)
-BLACK = (181, 136, 99)
+# --- SETTINGS ---
 LEARNING_RATE = 0.1
-
+DISCOUNT_FACTOR = 0.95
+WHITE_PIECES = {'♙', '♖', '♘', '♗', '♕', '♔'}
+BLACK_PIECES = {'♟', '♜', '♞', '♝', '♛', '♚'}
 PIECE_VALUES = {
     '♙': 1, '♘': 3, '♗': 3, '♖': 5, '♕': 9, '♔': 100,
     '♟': -1, '♞': -3, '♝': -3, '♜': -5, '♛': -9, '♚': -100
@@ -25,7 +23,6 @@ def get_initial_board():
         ['♖','♘','♗','♕','♔','♗','♘','♖']
     ]
 
-board = get_initial_board()
 learned_values = {}
 
 def load_brain():
@@ -33,99 +30,136 @@ def load_brain():
     try:
         with open("bot_brain.pkl", 'rb') as f:
             learned_values = pickle.load(f)
-    except FileNotFoundError:
+    except:
         learned_values = {}
 
 def save_brain():
     with open("bot_brain.pkl", 'wb') as f:
         pickle.dump(learned_values, f)
 
-def get_state_key(curr_board):
-    return "".join(["".join(row) for row in curr_board])
+def get_state_key(board):
+    return "".join(["".join(row) for row in board])
 
-def evaluate_board(curr_board):
-    score = sum(PIECE_VALUES.get(curr_board[r][c], 0) for r in range(8) for c in range(8))
-    state_key = get_state_key(curr_board)
-    score += learned_values.get(state_key, 0)
-    return score + random.uniform(-0.05, 0.05)
-
-def get_all_moves(curr_board, turn):
+def get_all_moves(board, turn):
     moves = []
     is_white = (turn == 'white')
+    enemy_pieces = BLACK_PIECES if is_white else WHITE_PIECES
+    friendly_pieces = WHITE_PIECES if is_white else BLACK_PIECES
+
     for r in range(8):
         for c in range(8):
-            p = curr_board[r][c]
-            if p == '.' or (is_white and p not in PIECE_VALUES) or (not is_white and p in PIECE_VALUES):
+            p = board[r][c]
+            if p == '.' or (is_white and p not in WHITE_PIECES) or (not is_white and p not in BLACK_PIECES):
                 continue
-            # Simplified valid move generation
-            directions = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < 8 and 0 <= nc < 8:
-                    moves.append((r, c, nr, nc))
+            
+            dirs = []
+            if p in {'♖', '♜', '♕', '♛'}: dirs += [(0,1),(0,-1),(1,0),(-1,0)]
+            if p in {'♗', '♝', '♕', '♛'}: dirs += [(1,1),(1,-1),(-1,1),(-1,-1)]
+            
+            if p in {'♘', '♞'}:
+                for dr, dc in [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]:
+                    nr, nc = r+dr, c+dc
+                    if 0<=nr<8 and 0<=nc<8 and board[nr][nc] not in friendly_pieces:
+                        moves.append((r,c,nr,nc))
+            elif p in {'♔', '♚'}:
+                for dr, dc in [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]:
+                    nr, nc = r+dr, c+dc
+                    if 0<=nr<8 and 0<=nc<8 and board[nr][nc] not in friendly_pieces:
+                        moves.append((r,c,nr,nc))
+            elif p == '♙':
+                if r > 0 and board[r-1][c] == '.': moves.append((r,c,r-1,c))
+                for dc in [-1, 1]:
+                    if r > 0 and 0 <= c+dc < 8 and board[r-1][c+dc] in BLACK_PIECES: moves.append((r,c,r-1,c+dc))
+            elif p == '♟':
+                if r < 7 and board[r+1][c] == '.': moves.append((r,c,r+1,c))
+                for dc in [-1, 1]:
+                    if r < 7 and 0 <= c+dc < 8 and board[r+1][c+dc] in WHITE_PIECES: moves.append((r,c,r+1,c+dc))
+            
+            for dr, dc in dirs:
+                nr, nc = r+dr, c+dc
+                while 0<=nr<8 and 0<=nc<8:
+                    if board[nr][nc] == '.': moves.append((r,c,nr,nc))
+                    elif board[nr][nc] in enemy_pieces:
+                        moves.append((r,c,nr,nc))
+                        break
+                    else: break
+                    nr, nc = nr+dr, nc+dc
     return moves
 
-def select_best_move(curr_board, turn, move_count=0):
-    moves = get_all_moves(curr_board, turn)
+def evaluate_board(board, state_key=None):
+    if not state_key: state_key = get_state_key(board)
+    material = sum(PIECE_VALUES.get(board[r][c], 0) for r in range(8) for c in range(8))
+    knowledge = learned_values.get(state_key, 0)
+    return material + knowledge
+
+def select_move(board, turn, epsilon):
+    moves = get_all_moves(board, turn)
     if not moves: return None
 
-    if move_count == 0:
-        if turn == 'white':
-            openings = [(6, 4, 4, 4), (6, 3, 4, 3), (6, 2, 4, 2)]
-        else:
-            openings = [(1, 4, 3, 4), (1, 3, 3, 3), (1, 2, 3, 2)]
-        valid_openings = [m for m in moves if m in openings]
-        if valid_openings:
-            return random.choice(valid_openings)
+    if random.random() < epsilon:
+        return random.choice(moves)
 
     scored_moves = []
     for m in moves:
         sr, sc, er, ec = m
-        orig_p, dest_p = curr_board[sr][sc], curr_board[er][ec]
-        curr_board[er][ec], curr_board[sr][sc] = orig_p, '.'
-        move_score = evaluate_board(curr_board)
-        scored_moves.append((move_score, m))
-        curr_board[sr][sc], curr_board[er][ec] = orig_p, dest_p
-        
+        orig, dest = board[sr][sc], board[er][ec]
+        board[er][ec], board[sr][sc] = orig, '.'
+        score = evaluate_board(board)
+        scored_moves.append((score, m))
+        board[sr][sc], board[er][ec] = orig, dest
+    
     scored_moves.sort(key=lambda x: x[0], reverse=(turn == 'white'))
-    top_n = min(3, len(scored_moves))
-    return random.choice(scored_moves[:top_n])[1]
+    return scored_moves[0][1]
 
-def refresh_brain():
-    global learned_values
-    cleaned_brain = {k: v for k, v in learned_values.items() if abs(v) > 0.01}
-    for key in cleaned_brain:
-        if cleaned_brain[key] > 100: cleaned_brain[key] = 100
-        if cleaned_brain[key] < -100: cleaned_brain[key] = -100
-    learned_values = cleaned_brain
-    save_brain()
-
-def run_training_session(games=100):
-    global board
+def run_self_play_training(games=100):
     load_brain()
-    print(f"Starting training session: {games} games...")
-    for i in range(games):
+    epsilon = 0.5 
+
+    for g in range(games):
         board = get_initial_board()
         history = []
         turn = 'white'
-        for m_count in range(100):
-            move = select_best_move(board, turn, m_count)
-            if not move: break
-            board[move[2]][move[3]], board[move[0]][move[1]] = board[move[0]][move[1]], '.'
-            history.append(get_state_key(board))
-            turn = 'black' if turn == 'white' else 'white'
-        
-        final_eval = evaluate_board(board)
-        for state in history:
-            old_val = learned_values.get(state, 0)
-            learned_values[state] = (old_val * (1 - LEARNING_RATE)) + (final_eval * LEARNING_RATE)
-        
-        if (i + 1) % 10 == 0:
-            print(f"Game {i+1} complete. Brain size: {len(learned_values)}")
-            save_brain()
+        game_over = False
+        result_score = 0
+
+        for m_count in range(150):
+            move = select_move(board, turn, epsilon)
+            if not move: 
+                result_score = -5 
+                game_over = True
+                break
             
-    refresh_brain()
-    print("Training complete and memory refreshed.")
+            # Execute
+            board[move[2]][move[3]], board[move[0]][move[1]] = board[move[0]][move[1]], '.'
+            state_key = get_state_key(board)
+            history.append(state_key)
+
+            flat_board = "".join(["".join(row) for row in board])
+            if '♔' not in flat_board:
+                result_score = -50 
+                game_over = True
+                break
+            if '♚' not in flat_board:
+                result_score = 50
+                game_over = True
+                break
+
+            turn = 'black' if turn == 'white' else 'white'
+
+        if not game_over:
+            result_score = -10
+
+        for i, state in enumerate(reversed(history)):
+            reward = result_score * (DISCOUNT_FACTOR ** i)
+            old_val = learned_values.get(state, 0)
+            learned_values[state] = old_val + LEARNING_RATE * (reward - old_val)
+
+        if (g + 1) % 50 == 0:
+            print(f"Game {g+1} | Positions in Brain: {len(learned_values)}")
+            save_brain()
+            epsilon = max(0.1, epsilon * 0.99)
+
+    save_brain()
 
 if __name__ == "__main__":
-    run_training_session(100)
+    run_self_play_training(1000)
